d3d259a2a66aeafbf687d83941b4e59d40be2de7
zhanghongtong <zhanghongtong@foxmail.com>
2018-10-04 15:38:13 +0800

    init

---
+++ build.sh
@@ -0,0 +1,19 @@
+IMAGE_NAME=emqx-alpine-build-env
+CONTAINER_NAME=test
+VOLUMN_PATH=/opt
+EMQX_VERSION=v3.0-beta.4
+
+
+docker build -t ${IMAGE_NAME} ./emqx-alpine-build-env
+
+docker run -it --rm \
+	--name ${CONTAINER_NAME} \
+	-v $(pwd)/emqx-docker:${VOLUMN_PATH} \
+	${IMAGE_NAME} \
+	sh -c "git clone -b ${EMQX_VERSION} https://github.com/emqx/emqx-rel /emqx && cd /emqx  && make && cp -r  /emqx/_rel/emqx ${VOLUMN_PATH}"
+
+docker rmi ${IMAGE_NAME}
+
+docker build -t emqx:${EMQX_VERSION} ./emqx-docker
+
+docker save -o emqx-docker-${EMQX_VERSION}.zip emqx:${EMQX_VERSION}
